e2fe82d67be8b270aac3d62d3432593d041eb699
driazati <driazati@users.noreply.github.com>
2022-02-22 14:40:02 -0800

    Fix order of source / venv activate

--- jenkins/jenkins-jobs/update.sh
+++ jenkins/jenkins-jobs/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://opendev.org/jjb/jenkins-job-builder.git
 cd jenkins-job-builder
-source .venv/bin/activate
 virtualenv .venv
+source .venv/bin/activate
 pip install -r test-requirements.txt -e .
 python jenkins_jobs/__main__.py --user tvm-bot --password $1 --conf "/home/ubuntu/jenkins-jobs/$2/jenkins_jobs.ini" update "/home/ubuntu/jenkins-jobs/$2"
 
