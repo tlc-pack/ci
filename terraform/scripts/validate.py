74eee4996a51b32d5e350a5ccd06841abcae790a
Noah <konturn@wustl.edu>
2022-05-04 17:43:36 -0400

    finishing touches

--- terraform/scripts/validate.py
+++ terraform/scripts/validate.py
@@ -2,10 +2,10 @@ import sys
 import os
 from github import Github
 
-def check_is_verified(token, repository):
+def check_is_verified(token, repository, ref):
   client = Github(login_or_token=token)
   repo = client.get_repo(repository)
-  latest_commit = repo.get_commit("HEAD")
+  latest_commit = repo.get_commit(ref)
   return latest_commit.commit.raw_data["verification"]["verified"]
   
 if __name__ == '__main__' :
@@ -14,8 +14,9 @@ if __name__ == '__main__' :
 
   token = os.environ['GITHUB_TOKEN']
   working_repository = os.environ['PR_REPO_FULL_NAME']
+  pr_branch_name = os.environ['GITHUB_HEAD_REF']
 
-  is_verified = check_is_verified(token, working_repository)
+  is_verified = check_is_verified(token, working_repository, pr_branch_name)
   is_fork = not os.environ['GITHUB_REPOSITORY'] == working_repository
   is_deployer = os.environ['EMAIL'] in deployers
 
