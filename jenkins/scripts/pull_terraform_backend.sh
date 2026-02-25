24aea82d813d338f95b46a23c76f237e6607c045
Yong Wu <yongcale@gmail.com>
2026-02-24 20:08:44 -0800

    Update s3 buckets (#109)

--- jenkins/scripts/pull_terraform_backend.sh
+++ jenkins/scripts/pull_terraform_backend.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 export AWS_DEFAULT_REGION=us-west-2
-aws s3 cp s3://tvm-ci-terraform-state/env:/tvm-ci-prod/tfstate .
+aws s3 cp s3://tvm-ci-terraform-state-new/env:/tvm-ci-prod/tfstate .
 
 jq --raw-output .outputs.fleet_config.value > fleet_config.yaml \
   tfstate
